tradingstrategy-ai/frontend

commit 9ba05be31e82e067f590bde3e1bbeed3d2745aa3

fix strategy tile chart hover data
- caused by unexpected ChartIQ date/tz conversion (bug?)
- set dates using ISO date-part only
- reconstitute `originalDate` from ChartIQ's `DT` value
- close #596

src/lib/chart/ChartIQ.svelte
@@ -121,6 +121,8 @@ Dynamically ChartIQ modules (if available) and render chart element.
 				DateX: data && chartEngine.pixelFromDate(data.DT),
 				CloseY: data && chartEngine.pixelFromPrice(data.Close)
 			};
+			// ChartIQ doesn't preserve original UTC date; restore it from tz-adjusted DT value
+			data.originalDate = new Date(data.DT.getTime() - data.DT.getTimezoneOffset() * 60000);
 			cursor = { position, data };
 		});
 

src/routes/strategies/ChartThumbnail.svelte
@@ -36,7 +36,13 @@
 		return {
 			update() {
 				chartEngine.loadChart('strategy-thumbnail', {
-					masterData: data.map(([DT, Value]) => ({ DT, Value, Baseline: 0 })),
+					masterData: data.map(([date, value]) => ({
+						// passing ISO date (w/out time) to prevent buggy ChartIQ tz conversions
+						DT: date.toISOString().slice(0, 10),
+						Value: value,
+						// add baseline data for drawing a simple 0 basline series (see above)
+						Baseline: 0
+					})),
 					range: { dtLeft: startDate, dtRight: data.at(-1)?.[0] }
 				});
 
@@ -56,10 +62,8 @@
 			<Marker x={position.DateX} y={position.CloseY} size={4} />
 			<div class="chart-hover-info" style:--x="{position.cx}px" style:--y="{position.CloseY}px">
 				<UpDownCell value={data.Close - data.iqPrevClose}>
-					<div class="date">
-						<Timestamp date={data.DT} />
-					</div>
-					<div class="value">{formatPercent(data.Close)}</div>
+					<Timestamp date={data.originalDate} />
+					<div class="value">{formatPercent(data.Close, 2)}</div>
 				</UpDownCell>
 			</div>
 		{/if}
@@ -87,8 +91,7 @@
 		top: var(--y);
 		transform: translate(-50%, calc(-100% - var(--space-md)));
 
-		.date {
-			font: var(--f-ui-sm-medium);
+		:global(time) {
 			color: hsla(var(--hsl-text-extra-light));
 		}
 

lenz-0x/stock-near-svelte

commit 02709596487428553118cb30878f661938b8b3e7

fix ChartIQ hover bug
- regression introduced in 9ba05be31e82e067f590bde3e1bbeed3d2745aa3
- fixes #605

src/lib/chart/ChartIQ.svelte
@@ -115,14 +115,19 @@ Dynamically ChartIQ modules (if available) and render chart element.
 		chartEngine.append('headsUpHR', () => {
 			const tick = chartEngine.tickFromPixel(chartEngine.cx);
 			const data = chartEngine.chart.dataSet[tick];
+
 			const position = {
 				cx: chartEngine.cx,
 				cy: chartEngine.cy,
 				DateX: data && chartEngine.pixelFromDate(data.DT),
 				CloseY: data && chartEngine.pixelFromPrice(data.Close)
 			};
+
 			// ChartIQ doesn't preserve original UTC date; restore it from tz-adjusted DT value
-			data.originalDate = new Date(data.DT.getTime() - data.DT.getTimezoneOffset() * 60000);
+			if (data) {
+				data.originalDate = new Date(data.DT.getTime() - data.DT.getTimezoneOffset() * 60000);
+			}
+
 			cursor = { position, data };
 		});
 